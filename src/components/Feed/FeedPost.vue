184f45209c145d903ec06713ff24ce89fd712c9f
Sondai Smith <sondaismith@users.noreply.github.com>
2025-01-14 17:52:50 -0400

    Adds initial support for "image" Posts
    - Modified `FeedPost` to support the displaying of images. Still need to create a variable indicating that a Post is a "media" post.
    - Added 8 test images to `./assets/test-media/posts.

--- src/components/Feed/FeedPost.vue
+++ src/components/Feed/FeedPost.vue
@@ -2,40 +2,47 @@
     {{ void "feed content" }}
     <div class="w-full pb-2">
         {{ void "feed post" }}
-        <div class="flex flex-col rounded bg-slate-400 p-1 w-full drop-shadow-md">
+        <div class="flex flex-col rounded bg-slate-400 p-1 w-full drop-shadow-md justify-between">
             {{ void "post pfp" }}
             <div class="flex w-full">
-                <div class="w-1/6">
+                <!-- <div class="w-1/6"> -->
+                <div>
                     <div class="rounded-full bg-stone-500 aspect-square size-10">
                         <i-mingcute:butterfly-2-line class="text-2xl h-full w-full p-1"/>
                     </div>
                 </div>
                 {{ void "post content" }}
-                <div class="pl-2 w-5/6">
-                    <div class="flex space-x-1 items-center">
+                <div class="flex flex-col pl-2 overflow-hidden">
+                    <div class="flex flex-shrink-0 space-x-1 items-center">
                         <div class="text-feedPostName font-semibold text-nowrap">Random User</div>
                         <div class="text-feedTimestamp truncate" title="@Random User hdahdhdaahd">@Random User hdahdhdaahd</div>
                         <div class="text-feedTimestamp text-nowrap cursor-pointer" @click="openPostDetails">1 Jan 2024</div>
                     </div>
-                    <div class="text-xs leading-4">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    <div class="text-xs leading-4 pb-2">
+                        Lorem ipsum dolor sit amet,
+                        sed ut labore among us magna
+                        aliqua. Quis nostrud exercitation
+                        ullamco laboris.
                     </div>
-                </div>
-            </div>
-            <div class="flex flex-row h-8">
-                <div class="w-1/6"></div>
-                <div class="w-5/6 pl-2 flex space-x-2">
-                    <div class="flex h-full items-center">
-                        <i-solar:chat-dots-outline class="text-base w-full"/>
-                        <div class="pl-1">2</div>
+                    {{ void "image-type media" }}
+                    <div class="flex w-fit border border-slate-700 rounded">
+                         <img class="w-fit" :src="getImageUrl(getRandomImageNum())"/>
                     </div>
-                    <div class="flex h-full items-center">
-                        <i-mingcute:repeat-line class="text-base"/>
-                        <div class="pl-1">23</div>
-                    </div>
-                    <div class="flex h-full items-center">
-                        <i-mingcute:heart-fill class="text-base"/>
-                        <div class="pl-1">30</div>
+                    <div class="flex flex-row h-8">
+                        <div class="flex space-x-2">
+                            <div class="flex h-full items-center">
+                                <i-solar:chat-dots-outline class="text-base w-full"/>
+                                <div class="pl-1">2</div>
+                            </div>
+                            <div class="flex h-full items-center">
+                                <i-mingcute:repeat-line class="text-base"/>
+                                <div class="pl-1">23</div>
+                            </div>
+                            <div class="flex h-full items-center">
+                                <i-mingcute:heart-fill class="text-base"/>
+                                <div class="pl-1">30</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -47,21 +54,34 @@
 import { defineComponent } from 'vue'
 import { postDetails } from '../../state/PostDetails.vue';
 
+
+function getImageUrl(num:String){
+    // var iNum = getRandomImageNum();
+    return new URL(`../../assets/test-media/posts/image${num}.png`, import.meta.url).href;
+}
+
+function getRandomImageNum(){
+    var imageNum = (Math.floor(Math.random()*8)+1);
+    console.log(imageNum);
+    return "0"+imageNum;
+}
+
 export default defineComponent({
     data(){
         return{
-            postDetails
+            postDetails,
+            getImageUrl,
+            getRandomImageNum,
         }
     },
     methods:{
         openPostDetails(){
             postDetails.showModal();
-        }
+        },
     },
     setup () {
-
         return {}
-    }
+    },
 })
 </script>
 
