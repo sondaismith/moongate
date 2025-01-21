9246601899b70bc7ad64daed0b41dc154edb644a
Sondai Smith <sondaismith@users.noreply.github.com>
2025-01-21 09:31:28 -0400

    Allows FeedColumn width to be adjusted
    - The width of `FeedColumn` components can be adjusted by dragging the right edge.
    - Added debug element that shows info relating to the resizing.
    - There is a known error with widths that cause the `FeedColumn` scrolling function to not work as expected because of the calculated target position not being a whole number (i.e. 190,5). Will fix in later commit.
    - This manual resize will eventually be replaced in a later commit as well, for an implementation that provides preset size options.

--- src/components/Feed/FeedColumn.vue
+++ src/components/Feed/FeedColumn.vue
@@ -1,5 +1,6 @@
 <template>
     {{ void "feed column" }}
+    <!-- <div class="flex"> -->
     <div :id="feedId" class="relative flex flex-col h-full w-72 bg-slate-900 overflow-hidden min-w-72 max-w-[600px]">
         {{ void "feed title" }}
         <div class="flex h-14 w-full border-b-2 border-white pl-2 pr-1 items-center">
@@ -25,10 +26,16 @@
             <!-- <FeedPost v-for="n in feedData?.totalPosts" /> -->
             <FeedPost v-for="n in PostCollection" :postData="n" />
             <div v-if="DebugFlags.showFeedColumnCenter" class="relative h-full w-0.5 left-1/2 bg-blue-900/60"></div>
+            <div class="absolute left-0 top-16 px-2 py-1 bg-orange-500/80 content-center">
+                <div>Dragging?: {{ isDragging }}</div>
+                <div>MousePos: {{ mousePosition }}</div>
+                <div>OriginalWidth: {{ originalWidth }}</div>
+            </div>
         </div>
         <div class="absolute pointer-events-none h-full left-0 right-0 border-2 rounded-sm border-sky-500/0 transition-colors"></div>
     </div>
-    <div data-test="feedColumn-resizer" draggable="true" @drag="resizeColumn()" class="bg-blue-900 w-1 cursor-ew-resize"></div>
+    <div data-test="feedColumn-resizer" @mousedown="startDrag($event)" class="relative bg-slate-900 w-1 cursor-ew-resize"></div>
+    <!-- </div> -->
 </template>
 
 <script lang="ts">
@@ -38,7 +45,7 @@ import { IFeedData } from '../../interfaces/FeedInterfaces';
 import { IPostDetails } from '../../interfaces/PostInterfaces';
 import { createPost } from '../../fake-data/PostFactory'
 
-var isDragging = true;
+var colElement;
 
 export default defineComponent({
     // props: {
@@ -52,6 +59,9 @@ export default defineComponent({
             lastUpdate: new Date(),
             DebugFlags,
             PostCollection: [] as IPostDetails[],
+            isDragging: false,
+            mousePosition: [0,0],
+            originalWidth: 0,
         }
     },
     props: {
@@ -80,20 +90,33 @@ export default defineComponent({
         refreshFeed(){
             this.addNewPost();
         },
-        startDrag(){
+        startDrag(e: MouseEvent){
             console.log('Drag start');
-            isDragging = true;
-            document.addEventListener('pointermove', this.resizeColumn);
-        },
-        resizeColumn(){
-            if(isDragging){
-                console.log('Dragging');
+            this.isDragging = true;
+            if(!this.feedId) return;
+            const column = document.getElementById(this.feedId);
+            if(column){
+                // this.originalWidth = column?.clientWidth;
+                colElement = column;
+                this.originalWidth = colElement.clientWidth;
             }
+            // this.originalWidth = e.clientX;
+            document.addEventListener('mousemove', this.resizeColumn);
+            document.addEventListener('mouseup', this.endDrag);
+        },
+        resizeColumn(e:MouseEvent){
+            // if(this.isDragging){
+            //     console.log('Dragging');
+            // }
+            this.mousePosition = [e.clientX,e.clientY];
+            (colElement as HTMLElement).style.width = ((colElement as HTMLElement).clientWidth+e.movementX)+"px";
         },
         endDrag(){
             console.log('Drag end');
-            // isDragging = false;
-            document.removeEventListener('mousemove', this.endDrag);
+            this.isDragging = false;
+            this.mousePosition = [0,0];
+            document.removeEventListener('mousemove', this.resizeColumn);
+            document.removeEventListener('mouseup', this.endDrag);
         }
     },
     created(){
