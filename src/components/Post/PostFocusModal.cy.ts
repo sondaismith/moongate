628af2b850c4685b3712768db48f351f095f7125
Sondai Smith <sondaismith@users.noreply.github.com>
2025-07-23 10:47:12 -0300

    Updates `PostFocusModal` cypress test -  Made changes to the Cypress test so that interacting with the component in the browser testing area navigates through the test data correctly. - Closes #141.

--- src/components/Post/PostFocusModal.cy.ts
+++ src/components/Post/PostFocusModal.cy.ts
@@ -1,15 +1,12 @@
 import PostFocusModal from './PostFocusModal.vue';
-import PostThreadView from './PostThreadView.vue';
-import postDetails from '../../state/PostDetails.vue'
-import { ThreadViewPost } from '@atproto/api/dist/client/types/app/bsky/feed/defs';
 
 const today = new Date();
 const testPostThreadView = [{
     $type:"app.bsky.feed.defs#threadViewPost",
     post: {
         author:{
-            did:'this-is-a-test-DID',
-            handle:'liltesty',
+            did:'did:plc:this-is-a-test-DID',
+            handle:'liltesty.test',
             displayName:'test dude'
         },
         cid:'this-is-a-test-CID',
@@ -18,18 +15,18 @@ const testPostThreadView = [{
             type: "app.bsky.feed.post",
             text: "Test record. Hello World!"
         },
-        uri:'this-is-a-test-URI'
+        uri:'at://did:plc:this-is-a-test-URI'
     },
     replies:[
         {
             $type:"app.bsky.feed.defs#threadViewPost",
             post:{
                 author:{
-                    did:'reply01-test-DID',
-                    handle:'reply01',
+                    did:'did:plc:reply01-test-DID',
+                    handle:'reply01.test',
                     displayName:'reply one'
                 },
-                cid:'this-is-reply01-CID',
+                cid:'bafyreihecnca4as334ixhhfdltmigdlzelxiixdnj2uj4yylgmckfbua54',
                 indexedAt: new Date(today.getTime()-52*60000).toISOString(),
                 record:{
                     type: "app.bsky.feed.post",
@@ -39,29 +36,30 @@ const testPostThreadView = [{
                 replyCount:1,
                 repostCount:10,
                 likeCount:2,
-                uri:'this-is-reply01-URI'
+                uri:'at://did:plc:this-is-reply01-URI'
             },
             replies:[
                 {
                     $type:"app.bsky.feed.defs#threadViewPost",
                     post:{
                         author:{
-                            did:'reply01-reply01-test-DID',
-                            handle:'reply01-of-reply01',
+                            did:'did:plc:reply01-reply01-test-DID',
+                            handle:'reply01-of-reply01.test',
                             displayName:'reply one of reply one'
                         },
-                        cid:'this-is-reply01-reply01-CID',
+                        cid:'bafyreihecnca4as334ixhhfdltmigdlzelxiixdnj2uj4yylgmckfbua54',//cid:'this-is-reply01-reply01-cid',
                         indexedAt: new Date(today.getTime()-44*60000).toISOString(),
                         record:{
                             type: "app.bsky.feed.post",
                             text: "Test reply 1 to reply 1. Hello World!",
                             createdAt: new Date(today.getTime()-44*60000).toISOString()
                         },
-                        replyCount:2,
+                        replyCount:0,
                         repostCount:0,
                         likeCount:1337,
-                        uri:'this-is-reply01-reply01-URI'
+                        uri:'at://did:plc:this-is-reply01-reply01-URI'
                     },
+                    replies:[]
                 }
             ]
         },
@@ -69,11 +67,11 @@ const testPostThreadView = [{
             $type:"app.bsky.feed.defs#threadViewPost",
             post:{
                 author:{
-                    did:'reply02-test-DID',
-                    handle:'reply02',
+                    did:'did:plc:reply02-test-DID',
+                    handle:'reply02.test',
                     displayName:'reply two'
                 },
-                cid:'this-is-reply02-CID',
+                cid:'bafyreihecnca4as334ixhhfdltmigdlzelxiixdnj2uj4yylgmckfbua54',//cid:'this-is-reply02-CID',
                 indexedAt: new Date(today.getTime()-50*60000).toISOString(),
                 record:{
                     type: "app.bsky.feed.post",
@@ -83,25 +81,45 @@ const testPostThreadView = [{
                 replyCount:0,
                 repostCount:765,
                 likeCount:22,
-                uri:'this-is-reply02-URI'
+                uri:'at://did:plc:this-is-reply02-URI'
             },
+            replies:[]
         }
     ]
 }];
 
+/**Helper method used to simulate Bluesky API call. Used to search
+ * testPostThreadView test object to return relevant Post/Reply.
+ */
+function findTestReply(data:Array<any>, postUri:string){
+    for(const item of data){
+        const result = item.post.uri == postUri ? item : findTestReply(item.replies, postUri);
+        if(result) return result;
+    }
+}
+
 describe('Clicking timestamp requests data', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-vue
+    //Simulate API call and return matching Post/Reply from test data (testPostThreadView)
+    cy.intercept('GET','https://api.bsky.app/xrpc/app.bsky.feed.getPostThread*', (req) => {
+        //DEBUG
+        // console.log(req);
+        // console.log(req.query.uri);
+        // console.log(findTestReply(testPostThreadView,req.query.uri.toString()));
+        req.reply({
+            body: {thread:findTestReply(testPostThreadView,req.query.uri.toString())},
+            statusCode: 200
+        });
+        console.log(req);
+    }).as('getPostThreadTest');
     cy.mount(PostFocusModal,{
         data() {
             return{
-                postDetails:{
-                    postThread: testPostThreadView[0],
-                    threadNavHistory: testPostThreadView,
-                    currentThreadView: testPostThreadView[0],
-                    whoCanReply: () => {return 'Everybody can reply'},
-                    setThreadContext: (newThread:ThreadViewPost) => {}
-                }
+                postThread: testPostThreadView[0],
+                threadNavHistory: testPostThreadView,
+                currentThreadView: testPostThreadView[0],
+                whoCanReply: () => {return 'Everybody can reply'},
             }
         },
     }).then(async ({ wrapper, component }) => {
