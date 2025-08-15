72647c6b18d65ae5402de605340ca21d7039f94a
Sondai Smith <sondaismith@users.noreply.github.com>
2025-08-15 12:34:02 -0300

    Updates `PostFocusModal` to support image navigation via keyboard
    - Updated `PostFocusModal` and related components `SquareButton` and `SlideshowArrow` to support navigating through displayed images via the keyboard.
    - Also updated the close button so that it can be interacted with/navigated to via keyboard.

--- src/components/Post/PostFocusModal.vue
+++ src/components/Post/PostFocusModal.vue
@@ -15,10 +15,12 @@
         <div class="relative flex flex-col w-full sm:w-3/5 grow min-h-[30rem]">
             <div class="h-10 w-full shrink-0 bg-blacks">
                 {{ void "Close Button" }}
-                <div data-test="postFocusModal-close-button" @click="hideModal" class="flex shrink-0 text-primary bg-btn rounded-br items-center aspect-square w-10
-                    justify-center text-2xl cursor-pointer sm:ml-auto sm:rounded-bl sm:rounded-br-none">
+                <SquareButton data-test="postFocusModal-close-button" @click="hideModal"
+                class="text-primary bg-btn !rounded-br aspect-square w-10 text-2xl
+                sm:ml-auto sm:!rounded-tl-none sm:!rounded-r-none"
+                button-padding="0">
                     <i-mingcute:close-fill/>
-                </div>
+                </SquareButton>
             </div>
             {{ void "Media Container" }}
             <div class="flex items-center h-full w-full justify-center overflow-hidden">
@@ -187,6 +189,7 @@ import { HandleAPIError } from '../../helpers/errors';
 import ImageContainer from '../Utilities/ImageContainer.vue';
 import SlideshowArrow from '../Utilities/SlideshowArrow.vue';
 import { ViewExternal } from '@atproto/api/dist/client/types/app/bsky/embed/external';
+import SquareButton from '../Utilities/SquareButton.vue';
 
 export default defineComponent({
     components:{
@@ -199,6 +202,7 @@ export default defineComponent({
         EmbedExternal,
         SlideshowArrow,
         VerifiedBadge,
+        SquareButton,
     },
     props:{
         // /**
--- src/components/Utilities/SquareButton.vue
+++ src/components/Utilities/SquareButton.vue
@@ -1,10 +1,11 @@
 <template>
-    <button class="group flex rounded cursor-pointer justify-center items-center gap-1 transition-colors"
+    <button class="group !border-transparent flex rounded cursor-pointer justify-center items-center gap-1 transition-colors"
     :class="[$attrs.class ? $attrs.class : 'bg-sky-500 hover:bg-sky-700 hover:border-sky-700',
         isDisabled ? '!bg-gray-600 text-gray-400 pointer-events-none select-none' : ''
     ]">
-        <div class="border-2 p-2 border-transparent group-focus:border-feedtypeBtnFocusHighlight
-        w-full"
+        <div class="flex border-2 border-transparent group-focus:border-feedtypeBtnFocusHighlight
+        items-center"
+        :class="`p-${buttonPadding}`"
         style="border-radius: inherit;">
             <slot></slot>
         </div>
@@ -17,9 +18,17 @@ import { defineComponent } from 'vue'
 export default defineComponent({
     name:"SquareButton",
     props:{
+        /**Is the button disabled? */
         isDisabled:{
             type:Boolean
         },
+        /**
+         * The padding to use around the button content. Default is 2 (applies 'p-2').
+         */
+        buttonPadding:{
+            type:String,
+            default:'2'
+        }
     },
     setup () {
         return {}
