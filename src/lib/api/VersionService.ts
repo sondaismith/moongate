49bc8ce79bc661896fe560fb5b5c6a46e7d4b000
Sondai Smith <sondai.smith@gmail.com>
2025-10-20 13:31:33 +0100

    Adds `update-version` file that generates build details
    - Added `update-version.cjs` script that will generate build details (version number, commit hash, build date) so that they can be displayed on application's "About App" page. This script is called before the build processes.
    - The generated build details are displayed in the application using the new `VersionService.ts` that was also created.

---
+++ src/lib/api/VersionService.ts
@@ -0,0 +1,17 @@
+import {version,buildDate,commitHash} from "../../helpers/version"
+
+export interface IVersionDetails{
+    version:string,
+    buildDate:string,
+    commitHash:string,
+}
+
+export function GetVersion():IVersionDetails{
+    let inDevEnv = import.meta.env.DEV;
+    let versionText = inDevEnv ? `${version}_dev` : version;
+    return {
+        version:versionText,
+        buildDate,
+        commitHash
+    };
+}
